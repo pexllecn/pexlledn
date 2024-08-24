4cd6c02d31f090af116e2844a7ed4a64ac61cc93
Kay <134864483+pexllecn@users.noreply.github.com>
2024-08-25 00:58:03 +0100

    updates

--- src/app/(demo)/categories/page.tsx
+++ src/app/(demo)/categories/page.tsx
@@ -1,5 +1,6 @@
 "use client";
-import sampleData from "@/data/sampleData.json";
+import AdCard from "./AdCard";
+import sampleData from "./sampleData.json";
 import React, { useState, useEffect, useCallback, useMemo } from "react";
 import { motion, AnimatePresence } from "framer-motion";
 import {
@@ -29,7 +30,6 @@ import {
   DollarSignIcon,
   SearchIcon
 } from "lucide-react";
-import AdCard from "./adcard";
 
 const variants1 = {
   hidden: { filter: "blur(10px)", opacity: 0 },
