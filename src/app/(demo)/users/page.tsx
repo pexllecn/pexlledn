ab8cb0e9f2e2284b9f9bcecd994d2f548c68d299
Kay <134864483+pexllecn@users.noreply.github.com>
2024-09-03 23:40:55 +0100

    Update page.tsx

--- src/app/(demo)/users/page.tsx
+++ src/app/(demo)/users/page.tsx
@@ -48,7 +48,7 @@ export default function UsersPage() {
         />
 
         {view === "grid" ? (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="px-3 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
             {paginatedUsers.map((user) => (
               <UserCard
                 key={user.id}
