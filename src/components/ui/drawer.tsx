6083555de3291b27376067a7c40964bee87625e7
Kay <134864483+pexllecn@users.noreply.github.com>
2024-12-01 11:26:51 +0000

    Update drawer.tsx

--- src/components/ui/drawer.tsx
+++ src/components/ui/drawer.tsx
@@ -43,7 +43,7 @@ const DrawerContent = React.forwardRef<
     <DrawerPrimitive.Content
       ref={ref}
       className={cn(
-        "px-5 py-1 fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[20px] border bg-background",
+        "p-1 fixed inset-x-0 bottom-0 z-50 mt-24 flex h-auto flex-col rounded-t-[20px] border bg-background",
         className
       )}
       {...props}
