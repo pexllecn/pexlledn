0d739fd40d43325bd9c8fac040ceb99b50e13113
Kay <134864483+pexllecn@users.noreply.github.com>
2024-12-16 18:09:10 +0000

    updated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -29,7 +29,6 @@ export default function RootLayout({
             {children}
             <ZoomPreventer />
             <Toaster />
-            <ThemeCustomizer />
           </BreadcrumbProvider>
         </ThemeProvider>
       </body>
